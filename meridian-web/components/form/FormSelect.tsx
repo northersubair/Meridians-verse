4db5ae067a620fbbffc31c0fa1cec2638bb88719
Leothosine <tosirano007@gmail.com>
2026-06-21 07:49:30 +0100

    feat: add FormSelect primitive wrapping shadcn Select

---
+++ meridian-web/components/form/FormSelect.tsx
@@ -0,0 +1,82 @@
+'use client'
+
+import { type FieldPath, type FieldValues, useFormContext } from 'react-hook-form'
+
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+
+export interface SelectOption {
+  value: string
+  label: string
+}
+
+interface FormSelectProps<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+> {
+  name: TName
+  label: string
+  options: SelectOption[]
+  placeholder?: string
+  description?: string
+  disabled?: boolean
+}
+
+export function FormSelect<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+>({
+  name,
+  label,
+  options,
+  placeholder = 'Select an option',
+  description,
+  disabled,
+}: FormSelectProps<TFieldValues, TName>) {
+  const { control } = useFormContext<TFieldValues>()
+
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <Select
+            onValueChange={field.onChange}
+            defaultValue={field.value as string | undefined}
+            disabled={disabled}
+          >
+            <FormControl>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder={placeholder} />
+              </SelectTrigger>
+            </FormControl>
+            <SelectContent>
+              {options.map(opt => (
+                <SelectItem key={opt.value} value={opt.value}>
+                  {opt.label}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          {description && <FormDescription>{description}</FormDescription>}
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  )
+}
